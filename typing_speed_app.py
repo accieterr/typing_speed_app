ee7b42a803b69e377d77c5650c4801b47c33889e
Ashton Yu <59806593+accieterr@users.noreply.github.com>
2026-09-06 12:29:49 -0700

    Added JSON file with 300 most common english words, implemented random prompts by choosing 30 random words from english words set.

--- typing_speed_app.py
+++ typing_speed_app.py
@@ -1,8 +1,10 @@
 import time
+import json
+import random
 
 def run_test():
     print("\n"*20)
-    prompt = "The quick brown fox jumps over the lazy dog"
+    prompt = generate_prompt()
     print(f"Type the following sentence and press ENTER afterwards:\n\n{prompt}\n")
     start_time = time.time()
     user_input = input()
@@ -17,5 +19,17 @@ def run_test():
     accuracy = correct/len(prompt)
     print(f"Accuracy: {accuracy*100:.2f}%")
     
+
+def generate_prompt():
+    #loads 300 most common english words from JSON file
+    with open("en_300.json", "r") as file:
+        en_300 = json.load(file)
+    
+    words = []
+
+    for i in range(30):
+        words.append(en_300["words"][random.randint(0,299)]["englishWord"]) 
+
+    return ' '.join(words) 
 
 run_test()
