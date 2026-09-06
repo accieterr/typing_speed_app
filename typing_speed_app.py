3b75798b9ff96a8bd6ca64887a6c5e144d892d85
Ashton Yu <59806593+accieterr@users.noreply.github.com>
2026-09-05 22:15:51 -0700

    Added simple console based test for single sentence, implemented time it took as well as accuracy

---
+++ typing_speed_app.py
@@ -0,0 +1,21 @@
+import time
+
+def run_test():
+    print("\n"*20)
+    prompt = "The quick brown fox jumps over the lazy dog"
+    print(f"Type the following sentence and press ENTER afterwards:\n\n{prompt}\n")
+    start_time = time.time()
+    user_input = input()
+    end_time = time.time()
+
+    print(f"\nTime it took: {end_time-start_time:.2f} seconds")
+    correct = 0
+    for letter_a, letter_b in zip(prompt, user_input):
+        if letter_a == letter_b:
+            correct += 1
+    
+    accuracy = correct/len(prompt)
+    print(f"Accuracy: {accuracy*100:.2f}%")
+    
+
+run_test()
