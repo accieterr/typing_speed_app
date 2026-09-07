b5286851b7a263563405061a39c064222a11519d
Ashton Yu <59806593+accieterr@users.noreply.github.com>
2026-09-07 16:26:06 -0700

    Added options for length of test. Also fixed issue with cursor position

--- typing_speed_app.py
+++ typing_speed_app.py
@@ -4,14 +4,14 @@ import random
 import curses
 from curses import wrapper
 
-def generate_prompt():
+def generate_prompt(numWords):
     #loads 300 most common english words from JSON file
     with open("en_300.json", "r") as file:
         en_300 = json.load(file)
 
     #appends 30 randomly selected words to words
     words = []
-    for i in range(30):
+    for i in range(numWords):
         words.append(en_300["words"][random.randint(0,299)]["englishWord"]) 
 
     #returns prompt string
@@ -23,12 +23,27 @@ def run_test(stdscr):
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
-    stdscr.nodelay(True)
+    stdscr.clear()
+    stdscr.addstr("How many words would you like on the test?\n1. 15 words\n2. 30 words\n3. 60 words\n")
+    numWords = 0
+    while True:
+        user_key = stdscr.getkey()
+        if ord(user_key) == ord('1'):
+            numWords = 15
+            break
+        elif ord(user_key) == ord('2'):
+            numWords = 30
+            break
+        elif ord(user_key) == ord('3'):
+            numWords = 60
+            break
 
-    prompt = generate_prompt()
+    prompt = generate_prompt(numWords)
     start_time = time.time()
     user_input = []
     
+    stdscr.nodelay(True)
+
     while True:
         stdscr.clear()
         stdscr.addstr(f"Type the following sentence and press ENTER afterwards:\n\n{prompt}")
@@ -45,10 +60,12 @@ def run_test(stdscr):
         elapsed_time_min = ((max((end_time-start_time), 1))/60)
         raw_wpm = (len(user_input)/5) / elapsed_time_min
         accuracy = correct/max(1 , min(len(user_input), len(prompt)))
-        wpm = max(0 , raw_wpm - ((len(user_input)-correct) / elapsed_time_min))
+        wpm = (correct/5) / elapsed_time_min
         stdscr.addstr(4, 0, f"NET WPM: {wpm:.0f} wpm")
         stdscr.addstr(5, 0, f"RAW WPM: {raw_wpm:.0f} wpm")
         stdscr.addstr(6, 0, f"ACCURACY: {accuracy*100:.2f}%")
+
+        stdscr.move(2, len(user_input))
 
         try:
             user_key = stdscr.getkey()
@@ -76,6 +93,7 @@ def main(stdscr):
         user_key = stdscr.getkey()
         if ord(user_key) == 27:
             break
-        run_test(stdscr)
+        else:
+            run_test(stdscr)
 
 wrapper(main)
